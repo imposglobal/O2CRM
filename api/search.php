7013cd09df1fdc4fba99558687ac1f3151289355
imposglobal <rushikesh@imposglobal.com>
2024-02-29 19:22:55 +0530

    Validation | Alerts

--- api/search.php
+++ api/search.php
@@ -1,2 +1,18 @@
 <?php 
 require('db.php');
+
+function search_customer($conn, $input){
+    $sql = "SELECT * FROM customers WHERE email='$input' OR phone='$input'";
+        $stmt = $db->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Check if user exists and password is correct
+        if ($result->num_rows == 1) {
+            echo"Found";
+        }else{
+            echo"Not Found";
+        }
+        search_customer($conn, 'doodlodesign@gmail.com');
+}
