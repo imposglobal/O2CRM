afee5d3553204db61e2c611ad8f422b966c5b55b
imposglobal <rushikesh@imposglobal.com>
2024-03-07 14:52:17 +0530

    db

--- api/db.php
+++ api/db.php
@@ -1,7 +1,22 @@
 <?php
-function db () {
-    $conn = mysqli_connect ("localhost", "root", "", "o2plan");
-    return $conn;
+ob_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+function db() {
+    // Attempt to connect to the database
+    $conn = mysqli_connect("localhost", "doodlueg_leadflow", "leadflow@2024", "doodlueg_leadcrm");
+
+    // Check connection
+    if (!$conn) {
+        // If connection fails, handle the error
+        die("Connection failed: " . mysqli_connect_error());
     }
-    $conn = db();
+
+    return $conn;
+}
+
+// Call the db function to establish a connection
+$conn = db();
+
 ?>
+
