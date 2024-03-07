b831da610ba05179597332e15d8e6aa8c350e2ac
imposglobal <rushikesh@imposglobal.com>
2024-03-07 17:18:23 +0530

    favicon

--- index.php
+++ index.php
@@ -276,9 +276,8 @@ input[type=text]:placeholder {
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>Plus Admin</title>
-   
-    <link rel="shortcut icon" href="./assets/images/favicon.png" />
+    <title>LeadHub CRM</title>
+  
   </head>
   <body>
   <div class="wrapper fadeInDown">
