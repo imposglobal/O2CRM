f1e1ddccf749cb99a267ed4725f9474c9e9d848b
imposglobal <rushikesh@imposglobal.com>
2024-03-11 17:03:14 +0530

    cname

--- o2/view_customer.php
+++ o2/view_customer.php
@@ -84,7 +84,7 @@
           <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle" id="cname"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
@@ -130,7 +130,7 @@
         
         html += '</tbody>';
         html += '</table>';
-        
+        $("#cname").text(data.First_Name);
         // Update modal body with generated HTML
         $('#modalBody').html(html);
         
